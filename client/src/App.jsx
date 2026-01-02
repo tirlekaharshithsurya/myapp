293174aacd4a6422c8afe6994d80cf7c54b8ae67
tirlekaharshithsurya <tirlekaharshithsurya@gmail.com>
2026-01-02 06:22:27 +0000

    Second Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-
+import Card from "./pages/Card";
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/card" element={<Card />} />
     </Routes>
   );
 }
---
+++ client/src/pages/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Card.css";
+function Card() {
+  return (
+    <>
+      <div className="card">HOVER</div>
+    </>
+  );
+}
+
+export default Card;
